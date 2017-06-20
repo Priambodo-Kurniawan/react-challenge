23531647c3d7b3a432851696f5d6629dc7ef8f7f
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-06-20 20:54:56 +0700

    dynamic data and change state

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -9,10 +9,18 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      title: 'My React App'
+      title: 'Avatar Creator',
+      name: 'default',
+      size: 280,
+      borderRadius: 10,
+      type: 'robo',
     }
 
     this.getData = this.getData.bind(this);
+    this.changeType = this.changeType.bind(this);
+    this.changeName = this.changeName.bind(this);
+    this.changeSize = this.changeSize.bind(this);
+    this.changeBorderRadius = this.changeBorderRadius.bind(this);
   }
 
   componentWillMount() {
@@ -20,16 +28,38 @@ class App extends Component {
   }
 
   getData() {
-    axios.get('http://api.adorable.io/avatars/list', {headers: {'Access-Control-Allow-Origin': '*'}})
+    axios.get('https://api.adorable.io/avatars/list', {headers: {'Access-Control-Allow-Origin': '*'}})
     .then(function (response) {
       console.log(response.data);
     })
     .catch(function (error) {
       console.log(error);
     });
-
   }
 
+  changeType(type){
+    this.setState({
+      type: type,
+    })
+  }
+
+  changeName(name){
+    this.setState({
+      name: name,
+    })
+  }
+
+  changeSize(size){
+    this.setState({
+      size: size,
+    })
+  }
+
+  changeBorderRadius(value){
+    this.setState({
+      borderRadius: value,
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -38,10 +68,37 @@ class App extends Component {
           <h2 className="title is-1">{this.state.title}</h2>
           <Row>
             <Col s={6}>
-              <AvatarForm />
+              <AvatarForm
+                changeType={this.changeType}
+                changeName={this.changeName}
+                changeSize={this.changeSize}
+                changeBorderRadius={this.changeBorderRadius}
+                size={this.state.size}
+                borderRadius={this.state.borderRadius}
+                />
             </Col>
             <Col s={6} className="flex-center">
-              <img src="https://api.adorable.io/avatars/285/hello" />
+              { (this.state.type === 'urban') ? (
+                <img
+                src={`https://api.adorable.io/avatars/${this.state.size}/${this.state.name}`}
+                style={{
+                  borderRadius:`${this.state.borderRadius}%`,
+                  width:`${this.state.size}`,
+                  height:`${this.state.size}`,
+                  margin: 'auto'
+                }} alt='' />
+              ) : (
+                <img
+                src={`https://robohash.org/${this.state.name}`}
+                style={{
+                  borderRadius:`${this.state.borderRadius}%`,
+                  width:`${this.state.size}`,
+                  height:`${this.state.size}`,
+                  backgroundColor: '#313131',
+                  margin: 'auto',
+                }} alt='' />
+              )}
+              { (this.state.name !== 'default') ? <h5 className="center-align">Hello <strong>{this.state.name}</strong>!</h5> : null }
             </Col>
           </Row>
           <div className="columns">
--- my-app/src/components/AvatarForm.js
+++ my-app/src/components/AvatarForm.js
@@ -3,28 +3,38 @@ import {Row, Input, Col} from 'react-materialize'
 
 
 class AvatarForm extends React.Component {
+
   render() {
     return (
       <div className="avatar-form">
         <Row>
-          <Input s={12} label="Your Name" validate></Input>
+          <Input s={12} label="Your Name" validate onChange={(e) => this.props.changeName(e.target.value)}></Input>
         </Row>
         <Row>
           <Col s={12}>
             <span className="label">Size</span>
-            <span className="results-label">285px</span>
+            <span className="results-label">{this.props.size}px</span>
             <p className="range-field">
-            <input type="range" id="test5" min="0" max="100" />
+            <input type="range" id="test5" min="100" max="285" onChange={(e) => this.props.changeSize(e.target.value)} />
             </p>
           </Col>
         </Row>
         <Row>
           <Col s={12}>
             <span className="label">Border Radius</span>
-            <span className="results-label">285px</span>
+            <span className="results-label">{this.props.borderRadius}%</span>
             <p className="range-field">
-            <input type="range" id="test5" min="0" max="100" />
+            <input type="range" id="test5" min="0" max="50" onChange={(e) => this.props.changeBorderRadius(e.target.value)} />
             </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col s={12}>
+            <span className="label">Avatar Type</span>
+            <div className="typeRadio">
+              <Input name='type' type='radio' value='urban' label='Urban' className='with-gap' onClick={(e) => this.props.changeType(e.target.value)} />
+              <Input name='type' type='radio' value='robo' label='Robo' className='with-gap' onClick={(e) => this.props.changeType(e.target.value)} />
+            </div>
           </Col>
         </Row>
       </div>
